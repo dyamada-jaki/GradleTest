ba231dfbf14ac6970418ce8f8d8545bcef0aa9c9
edy jaki <edy.jaki@gmail.com>
2016-01-22 00:21:05 +0900

    新規作成

---
+++ GradleTest01/src/main/java/jp/jaki/Product01.java
@@ -0,0 +1,9 @@
+package jp.jaki;
+
+public class Product01 {
+
+	public int add(int a, int b){
+		return a + b;
+	}
+
+}
---
+++ GradleTest01/src/test/java/jp/jaki/TestProduct01.java
@@ -0,0 +1,18 @@
+package jp.jaki;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Test;
+
+public class TestProduct01 {
+
+	@Test
+	public void testCase01() {
+		Product01 p01 = new Product01();
+		
+		int res = p01.add(1, 2);
+		assertThat(res, is(3));
+	}
+
+}
